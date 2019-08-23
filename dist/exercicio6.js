cbe06be0bb80990d3f7fdcc866c44cc29d7f6a22
gustavo xavier <gus.guilhermexavier08@gmail.com>
2019-08-23 20:41:37 -0300

    exercicio6

---
+++ dist/exercicio6.js
@@ -0,0 +1,6 @@
+var largura = 4;
+var comprimento = 7;
+var calc = function (largura, comprimento) {
+    return (largura * comprimento);
+};
+console.log("O RESULTADO E " + calc(largura, comprimento));
---
+++ exercicio6.ts
@@ -0,0 +1,17 @@
+
+let largura :number =4
+let comprimento :number =7
+
+
+let calc  = function(largura:number,comprimento:number){
+
+    
+return (largura*comprimento)
+  
+
+}
+
+console.log(`O RESULTADO E ${calc(largura,comprimento)}`)
+
+
+
